9b376ae11cb332ecff3cda23f9ecbdc959a9a4d4
krisdone <krisdone@rocketmail.com>
2019-12-11 18:08:50 +0100

    adding more stuff and testing source control

---
+++ prettyGolem/Startup.cs
@@ -0,0 +1,18 @@
+﻿// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace app
+{
+    public class start
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            //stuff goes here
+        }
+    }
+}
---
+++ utilities/connectKraken.cs
@@ -0,0 +1,20 @@
+﻿/* helpfull links below 
+ * https://www.kraken.com/features/api#ws-auth
+ * https://github.com/m4cx/kraken-wsapi-dotnet <-- TODO: buy him coffee :)
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+
+namespace krakenAPI
+{
+    public class connect
+    {
+		var client = KrakenApi.ClientFactory.Create("wss://ws-beta.kraken.com");
+        
+	}
+}
