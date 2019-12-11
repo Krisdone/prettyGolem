3e4b329285e8945b4068ba60f6a4383bafcdcb2f
krisdone <krisdone@rocketmail.com>
2019-12-11 18:23:23 +0100

    using different API

--- utilities/connectKraken.cs
+++ utilities/connectKraken.cs
@@ -1,6 +1,6 @@
-﻿/* helpfull links below 
- * https://www.kraken.com/features/api#ws-auth
- * https://github.com/m4cx/kraken-wsapi-dotnet <-- TODO: buy him coffee :)
+﻿/* helpfull API link below
+ * https://www.kraken.com/features/api#example-api-code-cs
+ * https://bitbucket.org/arrivets/krakenapi/src/master/README.txt
  */
 
 using System;
@@ -8,13 +8,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-
+using KrakenClient;
+using Jayrock.Json;
+using Jayrock.Json.Conversion;
 
 namespace krakenAPI
 {
-    public class connect
-    {
-		var client = KrakenApi.ClientFactory.Create("wss://ws-beta.kraken.com");
-        
-	}
+
 }
